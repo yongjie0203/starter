b4a0ceaac3ae21dfbe587a5e18bf200e4dc44c72
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-06 11:23:00 +0800

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/ssr.sh /etc/init.d/
-sudo chmod 755 /etc/init.d/ssr.sh
+sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/ssr.sh 
+sudo chmod 755 ssr.sh
+sudo mv ssr.sh /etc/init.d/
 sudo update-rc.d /etc/init.d/ssr.sh defaults 90
 
