ec389db7c23172fb240d7ec2a2990f0b4fcd10ab
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-06 11:19:20 +0800

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/ssr.sh /etc/init.d/
+sudo chmod 755 /etc/init.d/ssr.sh
+sudo update-rc.d /etc/init.d/ssr.sh defaults 90
 
-sh /root/shadowsocksr/logrun.sh
-
-exit 0
