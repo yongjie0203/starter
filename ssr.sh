473d66ee65c5fd38070d8867d31e366ee92872ea
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-06 11:45:16 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides:          ssr.sh
+# Required-start:    $local_fs $remote_fs $network $syslog
+# Required-Stop:     $local_fs $remote_fs $network $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the shadowsocksr daemon
+# Description:       starts shadowsocksr using start-stop-daemon
+### END INIT INFO
 sh /root/shadowsocksr/logrun.sh
   
 exit 0
