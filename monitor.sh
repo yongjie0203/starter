019e552f569a9c9dc1a55dd0106f5139161c28fa
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-10 16:04:52 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -6,5 +6,5 @@ number=`ps -ef | grep $proc_name | grep -v grep | wc -l`
 
 if [ $number -eq 0 ]                             # 判断进程是否存在
 then
-       sh /root/              # 重启进程的命令，请相应修改
+       sh /root/shadowsocksr/run.sh             # 重启进程的命令，请相应修改
 fi
