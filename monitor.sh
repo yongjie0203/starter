f0c5658487869053f2345cbf51c2deaf2ce02783
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-14 10:52:37 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 proc_name="server.py"                             # 待监控进程名
-
+echo date >> /root/test.log
 number=`ps -ef | grep $proc_name | grep -v grep | wc -l`
 
 if [ $number -eq 0 ]                             # 判断进程是否存在
