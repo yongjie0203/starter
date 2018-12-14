73dcb84642dd01a3b9679e55f18abcc276c7f113
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-14 10:41:34 +0800

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+cd /root/
 sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/ssr.sh 
 sudo chmod 755 ssr.sh
 sudo mv ssr.sh /etc/init.d/ssr.sh
 cd /etc/init.d/
 sudo update-rc.d ssr.sh defaults 90
-
+cd /root/
+sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/monitor.sh
+sudo chmod 755 monitor.sh
+sudo echo "1 * * * * root sh /root/monitor.sh" >> /etc/crontab
+sudo service cron restart
