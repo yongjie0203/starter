43182eac1c7d4fb69bb042b70d806b607ca01309
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2018-12-18 23:41:56 +0800

    添加更改时区，同步时间，每日重启

--- starter.sh
+++ starter.sh
@@ -6,8 +6,12 @@ sudo mv ssr.sh /etc/init.d/ssr.sh
 cd /etc/init.d/
 sudo update-rc.d ssr.sh defaults 90
 cd /root/
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+apt-get install ntp
+ntpdate cn.pool.ntp.org
 sudo wget https://raw.githubusercontent.com/yongjie0203/starter/master/monitor.sh
 sudo chmod 755 monitor.sh
+sudo echo "0 17 * * * root /sbin/reboot" >> /etc/crontab
 sudo echo "1 * * * *  root sh /root/monitor.sh" >> /etc/crontab
 sudo service cron restart
 cat /etc/crontab
