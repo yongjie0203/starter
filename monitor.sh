eddc9a5b1be9d376188e7f48fcba5bb9d35e6080
张永杰 <32083342+yongjie0203@users.noreply.github.com>
2019-08-11 03:52:48 +0800

    更新写测试日志内容

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
 
 proc_name="server.py"                             # 待监控进程名
-echo date >> /root/test.log
-number=`ps -ef | grep $proc_name | grep -v grep | wc -l`
 
+number=`ps -ef | grep $proc_name | grep -v grep | wc -l`
+date >> /root/test.log
+echo $number >> /root/test.log
 if [ $number -eq 0 ]                             # 判断进程是否存在
 then
-       sh /root/shadowsocksr/run.sh             # 重启进程的命令，请相应修改
+       sh /root/shadowsocksr/logrun.sh             # 重启进程的命令，请相应修改
 fi
